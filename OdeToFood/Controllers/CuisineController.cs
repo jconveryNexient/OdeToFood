c316c1b650ee3288994b92c7abb32418322973d9
Matt Rose <mrose@nexient.com>
2015-08-04 12:18:00 -0400

    Push to Joe

---
+++ OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Filters;
+
+namespace OdeToFood.Controllers
+{
+    public class CuisineController : Controller
+    {
+        // GET: Cuisine
+        [Log]          
+        public ActionResult Search(string name = "french")
+        {
+            //throw new Exception("Something terrible has happened");
+            var message = Server.HtmlEncode(name);            
+            return Json(new { Message = message, Name = "Matt" }, JsonRequestBehavior.AllowGet);
+        }
+
+        //// GET: Cuisine 
+        //[HttpGet]       
+        //public ActionResult Search()
+        //{
+        //    //return Content(message);
+        //    //return RedirectToAction("Index", "Home", new { name = name });
+        //    //return RedirectToRoute("Default", new {controller = "Home", action = "About"});
+        //    //return File(Server.MapPath("~/Content/site.css"), "text/css");
+        //    return Content("Search");
+        //}
+    }
+}
---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
+        public ActionResult Index(string searchTerm = null)
+        {
+            //var controller = RouteData.Values["controller"];
+            //var action = RouteData.Values["action"];
+            //var id = RouteData.Values["id"];
+            //var message = String.Format("{0}::{1} {2}", controller, action, id);
+            // ViewBag.Message = message;
+
+            //var model = from r in _db.Restaurants
+            //            orderby r.Reviews.Average(review => review.Rating) descending 
+            //            select new RestaurantListViewModel
+            //            {
+            //                Id = r.Id,
+            //                Name = r.Name,
+            //                City = r.City,
+            //                Country = r.Country,
+            //                CountOfReviews = r.Reviews.Count()
+            //            };
+
+            var model = 
+                _db.Restaurants
+                    .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+                    .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
+                    .Take(10)
+                    .Select(r => new RestaurantListViewModel
+                    {
+                        Id = r.Id,
+                        Name = r.Name,
+                        City = r.City,
+                        Country = r.Country,
+                        CountOfReviews = r.Reviews.Count()
+                    });
+
+            return View(model);            
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+    }
+}
